795e403ef3b66b53f345d083e8c2f18b50e6cf5e
Shifana nasrin <shifananazrin15@gmail.com>
2024-01-30 19:28:11 +0530

    Implement ShareMealPage with user form, image picker, and submission logic

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,7 @@
 "use client"
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import { shareMeal } from '@/lib/acions';
 
 export default function ShareMealPage() {
@@ -14,7 +15,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+      <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -45,7 +46,8 @@ export default function ShareMealPage() {
           {/* IMAGE PICKER */}
           <ImagePicker  label='Your Image' name='image'/>
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            {/* <button type="submit">Share Meal</button> */}
+            <MealsFormSubmit/>
           </p>
         </form>
       </main>
